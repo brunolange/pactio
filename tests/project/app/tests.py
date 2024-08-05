076df92d685881f6e2420642e17dcc0a9f28e39b
Bruno Lange <blangeram@gmail.com>
2024-08-04 17:43:32 -0700

    First implementation of pactio

--- tests/project/app/tests.py
+++ tests/project/app/tests.py
@@ -5,17 +5,34 @@ from django.test import Client, TestCase
 
 
 class TestPactio(TestCase):
-    def test_simple_pact(self):
-        test_simple_pact(self.client)
+    def test_broken_pact(self):
+        test_broken_pact(self.client)
+
+    def test_maintained_pact(self):
+        test_maintained_pact(self.client)
 
 
-def test_simple_pact(client: Client):
-    response = client.post("/app/my_view", json={"a": 1})
-    assert response.status_code == HTTPStatus.OK
-    assert json.loads(response.body) == {
+def test_broken_pact(client: Client):
+    response = client.post(
+        "/app/my_view",
+        json.dumps({"a": 1}),
+        content_type="application/json",
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_maintained_pact(client: Client):
+    data = {
         "a_str": "hello",
         "an_int": 42,
         "a_bool": True,
         "a_float": 3.14,
         "a_list_of_ints": [1, 1, 3, 5, 8, 13, 21],
     }
+    response = client.post(
+        "/app/my_view",
+        json.dumps(data),
+        content_type="application/json",
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == data

8fb713f8303e54acb4eaed4762299f1f70c58600
Bruno Lange <blangeram@gmail.com>
2024-09-08 10:42:45 -0700

    Sets up failing test for accepting form data

--- tests/project/app/tests.py
+++ tests/project/app/tests.py
@@ -8,8 +8,11 @@ class TestPactioMiddleware(TestCase):
     def test_broken_pact(self):
         test_broken_pact(self.client)
 
-    def test_maintained_pact(self):
-        test_maintained_pact(self.client)
+    def test_maintained_pact_with_json(self):
+        test_maintained_pact_with_json(self.client)
+
+    def test_maintained_pact_with_form(self):
+        test_maintained_pact_with_form(self.client)
 
 
 def test_broken_pact(client: Client):
@@ -28,7 +31,20 @@ def test_broken_pact(client: Client):
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
-def test_maintained_pact(client: Client):
+def test_maintained_pact_with_form(client: Client):
+    data = {
+        "a_str": "hello",
+        "an_int": 42,
+        "a_bool": True,
+        "a_float": 3.14,
+        "a_list_of_ints": [1, 1, 3, 5, 8, 13, 21],
+    }
+    response = client.post("/app/echo", data=data)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == data
+
+
+def test_maintained_pact_with_json(client: Client):
     data = {
         "a_str": "hello",
         "an_int": 42,
